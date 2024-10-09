a8002332044fac650878f94bd999d7c75ea3af9b
davor.holender <davor.holender@cubes.rs>
2024-10-09 13:02:10 +0200

    test routing 3

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 /**
      * Google Search Console
      */
-    Route::middleware('can:gsc-cms')->name('google_search_console.')->namespace('Hoks\CMSGSC\Controllers')->prefix('/google-search-console')->group(function(){
+    Route::middleware('can:gsc-cms')->name('google_search_console.')->prefix('/google-search-console')->group(function(){
         Route::get('/{activeWebsite?}', 'GoogleSearchConsoleController@index')->name('index');
         Route::post('/datatable', 'GoogleSearchConsoleController@datatable')->name('datatable');
         Route::post('/pages-datatable', 'GoogleSearchConsoleController@pagesDatatable')->name('pages_datatable');
