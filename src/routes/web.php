2e8e41829fb5313ae1dfff62f03608582e6d9014
davor.holender <davor.holender@cubes.rs>
2024-10-09 11:25:36 +0200

    fix routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,23 +1,5 @@
 <?php
 
-/**
- * URLs - always kebab-cased ( http://wiki.c2.com/?KebabCase ), always begining with a '/' 
- *      - order of words in a route goes as follows: 
- *          #1 name of entity in plural (ex. "entities")
- *          #2 (optional) {entity} (if action is related to one entity) this parameter is the primary key of entity model
- *          #3 action name
- * 
- *      - entity's ID is always represented as {entity}, because of policies applied in controller
- * Names - underscore case, always
- * 
- * Grouping - by controller - minimum. In each group, a url prefix & name prefix 
- * are a must, the namspace prefix is optional if the controller is in a 
- * sub-folder.
- * 
- * example:
- */
-Auth::routes();
-
 Route::middleware(['auth'])->group(function () {
 /**
      * Google Search Console
