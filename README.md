569bb95ea471ccb5dc1d5dbb12c2c21fcbe51c65
davor.holender <davor.holender@cubes.rs>
2024-10-17 10:01:58 +0200

    fix(routes)

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Installation:
 1. composer require davor/cms-gsc
 2. php artisan vendor:publish --tag=config --provider="Hoks\CMSGSC\CMSGSCServiceProvider"
 3. php artisan vendor:publish --tag=views --provider="Hoks\CMSGSC\CMSGSCServiceProvider"
-4. php artisan vendor:publish --tag=controllers --provider="Hoks\CMSGSC\CMSGSCServiceProvider"
+4. run migrations
 5. Put service_account.json to desired location
 6. Set up config/gsc-cms.php
 7. Call command GetGoogleSearchConsoleData according to you needs (for example, once a day at 03:00)
 8. Make permission to access package pages (gsc-cms)
 9. put require base_path('vendor/davor/cms-gsc/src/routes/web.php'); inside of routes/web.php
-10. Use package :D
+10. Access the page /google-search-console
+11. Use package :D
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,10 +5,10 @@ use Hoks\CMSGSC\Controllers\GoogleSearchConsoleController;
      * Google Search Console
      */
     Route::middleware('can:gsc-cms')->name('google_search_console.')->prefix('/google-search-console')->group(function(){
-        Route::get('/{activeWebsite?}', 'GoogleSearchConsoleController@index')->name('index');
-        Route::post('/datatable', 'GoogleSearchConsoleController@datatable')->name('datatable');
-        Route::post('/pages-datatable', 'GoogleSearchConsoleController@pagesDatatable')->name('pages_datatable');
-        Route::post('/exclude/{query}', 'GoogleSearchConsoleController@toggleExclude')->name('toggle_exclude');
-        Route::post('/mark-as-fixed/{query}', 'GoogleSearchConsoleController@toggleFixed')->name('toggle_fixed');
-        Route::get('/pages/{query}', 'GoogleSearchConsoleController@pages')->name('pages');
+        Route::get('/{activeWebsite?}', [GoogleSearchConsoleController::class,'index'])->name('index');
+        Route::post('/datatable', [GoogleSearchConsoleController::class,'datatable'])->name('datatable');
+        Route::post('/pages-datatable', [GoogleSearchConsoleController::class,'pagesDatatable'])->name('pages_datatable');
+        Route::post('/exclude/{query}', [GoogleSearchConsoleController::class,'toggleExclude'])->name('toggle_exclude');
+        Route::post('/mark-as-fixed/{query}', [GoogleSearchConsoleController::class,'toggleFixed'])->name('toggle_fixed');
+        Route::get('/pages/{query}', [GoogleSearchConsoleController::class,'pages'])->name('pages');
     });
