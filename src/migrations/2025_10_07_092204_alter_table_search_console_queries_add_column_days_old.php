830b5a8de7520c32f67a7a823c563563b73bbace
Davor Holender <davor@Davors-MacBook-Pro.local>
2025-10-07 10:13:22 +0200

    dev, allow 1, 3, and 7 days old data, fix migrations

--- src/migrations/2025_10_07_092204_alter_table_search_console_queries_add_column_days_old.php
+++ src/migrations/2025_10_07_092204_alter_table_search_console_queries_add_column_days_old.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterTableSearchConsoleQueriesStatusesAddColumnDelegated extends Migration
+class AlterTableSearchConsoleQueriesStatusesAddDaysOld extends Migration
 {
     /**
      * Run the migrations.
